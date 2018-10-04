e9073bfb7ac0fc8c24c3e34e233de8bb10f3269a
jaykhedekar7 <jaykhedekar@hotmail.com>
2018-10-04 20:13:11 +0530

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,27 @@
+var poster1 = document.getElementById("poster1");
+var poster2 = document.getElementById("poster2");
+var poster3 = document.getElementById("poster3");
+// var productimg = document.getElementById("productimage");
+
+
+poster2.addEventListener("click", function(){
+    
+    window.location.href = "product.html";
+    // productimg.src = "img/p1.png";
+   
+});
+
+poster1.addEventListener("click", function(){
+    
+    window.location.href = "product.html";
+    // productimg.src = "img/p1.png";
+   
+});
+
+poster3.addEventListener("click", function(){
+    
+    window.location.href = "product.html";
+    // productimg.src = "img/p1.png";
+   
+});
+
